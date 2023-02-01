ab284e0f5e9ebeeeac492c5c2a07fb81e37250c7
Prasadrpatil <prasadrpatil25.10@gmail.com>
2023-02-01 09:57:15 +0530

    first commit

---
+++ eks-worker-nodes.tf
@@ -0,0 +1,71 @@
+#
+# EKS Worker Nodes Resources
+#  * IAM role allowing Kubernetes actions to access other AWS services
+#  * EKS Node Group to launch worker nodes
+#
+
+resource "aws_iam_role" "demo-node" {
+  name = "terraform-eks-node"
+  
+  tags = tomap({
+    "Environment"                               = "Dev",
+    "Project"                                   = "FT",
+  })
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.demo-node.name
+}
+
+resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.demo-node.name
+}
+
+resource "aws_iam_role_policy_attachment" "demo-node-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.demo-node.name
+}
+
+resource "aws_eks_node_group" "demo" {
+  cluster_name    = aws_eks_cluster.demo.name
+  node_group_name = "demo"
+  node_role_arn   = aws_iam_role.demo-node.arn
+  subnet_ids      = aws_subnet.demo[*].id
+  instance_types  = ["t2.small"]
+  disk_size       = 10
+  
+  tags = tomap({
+    "Environment"                               = "Dev",
+    "Project"                                   = "FT",
+  })
+
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.demo-node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.demo-node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.demo-node-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
